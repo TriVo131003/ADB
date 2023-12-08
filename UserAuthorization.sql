f8f9dc1836f57f65423ba9b071377881d60ec7bb
HieuTran91 <91721815+HieuTran91@users.noreply.github.com>
2023-12-08 19:16:57 +0700

    vetify user

---
+++ UserAuthorization.sql
@@ -0,0 +1,53 @@
+﻿USE QLPHONGKHAM
+GO
+
+--tạo user với login tương ứng
+CREATE USER [00020] FOR LOGIN PA
+
+-- thêm user vào role bệnh nhân
+sp_addrolemember 'Patient', '00020'
+
+-------------------------------------------
+--tạo role bác sĩ
+create role Dentist
+-- tạo login cho toàn bộ bác sĩ
+sp_addlogin 'DE', '123', 'QLPHONGKHAMNHAKHOA'
+sp_grantdbaccess 'DE', 'QLPHONGKHAMNHAKHOA'
+grant connect to guest
+
+--tạo user với login tương ứng
+CREATE USER [00015] FOR LOGIN DE
+
+-- thêm user vào role bác sĩ
+sp_addrolemember 'Dentist', '00015'
+
+-------------------------------------------
+--tạo role nhân viên
+create role Staff
+-- tạo login cho toàn bộ bệnh nhân
+sp_addlogin 'ST', '123', 'QLPHONGKHAMNHAKHOA'
+sp_grantdbaccess 'ST', 'QLPHONGKHAMNHAKHOA'
+grant connect to guest
+
+--tạo user với login tương ứng
+CREATE USER [00010] FOR LOGIN ST
+
+-- thêm user vào role NhanVien
+sp_addrolemember 'Staff', '00010'
+
+-------------------------------------------
+--tạo role admin
+create role [Admin]
+-- tạo login cho toàn bộ bệnh nhân
+sp_addlogin 'AD', '123', 'QLPHONGKHAMNHAKHOA'
+sp_grantdbaccess 'AD', 'QLPHONGKHAMNHAKHOA'
+grant connect to guest
+
+--tạo user với login tương ứng
+CREATE USER [00001] FOR LOGIN AD
+
+-- thêm user vào role NhanVien
+sp_addrolemember 'Admin', '00001'
+
+grant select, insert, update on Dentist_MedicalRecord to Dentist
+grant select, insert, update on Dentist_or_Patient_Person to Patient
