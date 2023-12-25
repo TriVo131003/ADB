455fd1f0578648dbe05df3c540f6a17ed9cd2989
Võ Cao Trí <trivo.131003@gmail.com>
2023-12-25 12:49:04 +0700

    fix

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,51 @@ def signup():
     print('get started')
     return render_template('signup.html')
 
+@app.route('/patientinfo', methods = ['POST','GET'])
+def patientinfo():
+    return render_template('patientinfo.html')
+
+@app.route('/addpatient', methods = ['POST','GET'])
+def addpatient():
+    return render_template('addpatient.html')
+
+@app.route('/updatepatient', methods = ['POST','GET'])
+def updatepatient():
+    return render_template('updatepatient.html')
+
+@app.route('/patientrecord', methods = ['POST','GET'])
+def patientrecord():
+    return render_template('patientrecord.html')
+
+@app.route('/treatmentplan', methods = ['POST','GET'])
+def treatmentplan():
+    return render_template('treatmentplan.html')
+
+@app.route('/treatmentplanlist', methods = ['POST','GET'])
+def treatmentplanlist():
+    return render_template('treatmentplanlist.html')
+
+@app.route('/allergycontracdication', methods = ['POST','GET'])
+def allergycontracdication():
+    return render_template('allergycontracdication.html')
+
+@app.route('/invoice', methods = ['POST','GET'])
+def invoice():
+    return render_template('invoice.html')
+
+@app.route('/drug', methods = ['POST','GET'])
+def drug():
+    return render_template('drug.html')
+
+@app.route('/adddrug', methods = ['POST','GET'])
+def adddrug():
+    return render_template('adddrug.html')
+
+@app.route('/updatedrug', methods = ['POST','GET'])
+def updatedrug():
+    return render_template('updatedrug.html')
+
+
 # @app.route('/appointmentinfo', methods = ['POST','GET'])
 # def appointmentinfo():
 #     return render_template('thongtincuochen.html')
